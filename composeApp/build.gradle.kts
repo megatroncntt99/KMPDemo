d6f0d7f3d1a5b989645cf26fd2831fe6f201bfd1
LAPTOP-IM369426\van <vannv8@fpt.com.vn>
2024-01-22 22:09:07 +0700

    hot fix

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -49,6 +49,8 @@ kotlin {
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
             implementation(libs.kotlinx.serialization.json)
+            api(libs.moko.mvvm.core)
+            api(libs.moko.mvvm.compose)
 
         }
         iosMain.dependencies {
